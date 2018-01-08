3bc800ce4bb265196046c702598616401d114482
kulisfunk <kulisfunk@hotmail.com>
2018-01-08 19:45:38 +0000

    created new react app modified app and created moviebox class that displays message

---
+++ imdb/src/components/MovieBox.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+class MovieBox extends React.Component {
+
+  render() {
+    return (
+      <div className="movie-box">
+       IMDB movie list
+      </div>
+    )
+  }
+
+}
+
+export default MovieBox;
