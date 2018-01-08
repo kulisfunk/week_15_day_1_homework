b938deba966de6e39e3a94839f53eaf57bdb26d7
kulisfunk <kulisfunk@hotmail.com>
2018-01-08 19:57:02 +0000

    created movie class and imported into movielist so it renders a message

---
+++ imdb/src/components/Movie.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class Movie extends React.Component {
+  render() {
+    return (
+      <div className="movie">
+       I am a movie
+      </div>
+    )
+  }
+}
+
+export default Movie;
--- imdb/src/components/MovieList.js
+++ imdb/src/components/MovieList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Movie from './Movie';
 
 
 class MovieList extends React.Component {
@@ -6,6 +7,7 @@ class MovieList extends React.Component {
     return (
     <div className="movie-list">
      this is a movie list
+     <Movie />
     </div>
   )}
 }
